5ae1ec16cd45ed283a85dec5a430d3aa11cc533e
Eduardo-dS3 <eduardo.santos.3538@gmail.com>
2025-08-25 21:54:02 -0300

    Tela de novo item

--- app/src/main/java/com/example/myapitest/MainActivity.kt
+++ app/src/main/java/com/example/myapitest/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.example.myapitest
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myapitest.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationPermissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +37,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         requestLocationPermission()
         setupView()
-
-        // 1- Criar tela de Login com algum provedor do Firebase (Telefone, Google)
-        //      Cadastrar o Seguinte celular para login de test: +5511912345678
-        //      Código de verificação: 101010
-
-        // 2- Criar Opção de Logout no aplicativo
 
         // 3- Integrar API REST /car no aplicativo
         //      API será disponibilida no Github
@@ -43,6 +57,10 @@ class MainActivity : AppCompatActivity() {
         binding.logoutCta.setOnClickListener{
             onLogout()
         }
+
+        binding.addCta.setOnClickListener{
+            navigateToNewCar()
+        }
     }
     private fun onLogout() {
         FirebaseAuth.getInstance().signOut()
@@ -53,11 +71,74 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun requestLocationPermission() {
-        // TODO
+        //Inicializar o FusedLocation
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        locationPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                getLastLocation()
+            } else {
+                Toast.makeText(this,
+                    R.string.permissao_de_localizacao_negada, Toast.LENGTH_SHORT).show()
+            }
+        }
+        checkLocationPermissionAndRequest()
+    }
+
+    private fun checkLocationPermissionAndRequest() {
+        when {
+            checkSelfPermission(ACCESS_FINE_LOCATION) == PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(
+                        this,
+                        ACCESS_COARSE_LOCATION
+                    ) == PERMISSION_GRANTED -> {
+                getLastLocation()
+            }
+
+            shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION) -> {
+                //Pedindo a permissão do usuário para ACCESS_FINE_LOCATION
+                locationPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+            }
+
+            shouldShowRequestPermissionRationale(ACCESS_COARSE_LOCATION) -> {
+                //Pedindo a permissão do usuário para ACCESS_COARSE_LOCATION
+                locationPermissionLauncher.launch(ACCESS_COARSE_LOCATION)
+            }
+
+            else -> {
+                //Fallback em caso de algum erro na verificação de permissão
+                locationPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
+    private fun getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocationPermission()
+            return
+        }
+        fusedLocationProviderClient.lastLocation.addOnCompleteListener { task: Task<Location> ->
+            if(task.isSuccessful){
+                val location = task.result
+                Log.d("Hello World", "Lat: ${location.latitude} Long: ${location.longitude}")
+            } else {
+                Toast.makeText(this, R.string.erro_desconhecido, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun fetchItems() {
         // TODO
+    }
+
+    private fun navigateToNewCar(){
+        startActivity(NewCarActivity.newIntent(this))
     }
 
     companion object {
---
+++ app/src/main/java/com/example/myapitest/NewCarActivity.kt
@@ -0,0 +1,99 @@
+package com.example.myapitest
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.myapitest.databinding.ActivityNewCarBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+
+class NewCarActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var binding: ActivityNewCarBinding
+    private lateinit var mMap: GoogleMap
+    private var selectedMarker: Marker? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewCarBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        setupView()
+        setupGoogleMap()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        binding.mapContent.visibility = View.VISIBLE
+        mMap.setOnMapClickListener { latLng ->
+            selectedMarker?.remove() //Limpa o atual caso exista
+
+            selectedMarker = mMap.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .draggable(true)
+                    .title("Lat: ${latLng.latitude}, Lng: ${latLng.longitude}")
+            )
+        }
+        getDeviceLocation()
+    }
+
+    private fun setupView() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+    }
+
+    private fun setupGoogleMap() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun getDeviceLocation() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PERMISSION_GRANTED
+        ) {
+            //Já tenho permissão de localização do usuário
+            loadCurrentLocation()
+        } else {
+            //
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun loadCurrentLocation() {
+        mMap.isMyLocationEnabled = true
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            val currentLocationLatLng = LatLng(location.latitude, location.longitude)
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocationLatLng, 15f))
+        }
+    }
+
+    companion object{
+
+        const val REQUEST_CODE_CAMERA = 101
+
+        fun newIntent(context: Context) = Intent(context, NewCarActivity::class.java)
+    }
+}
